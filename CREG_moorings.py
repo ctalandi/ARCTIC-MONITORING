92e69d41991933aefe3a0b9305504b932d6d7c29
claude.talandier <ctalandi@ifremer.fr>
2026-02-03 09:43:03 +0000

    switch MOORINGS scripts to python 3

--- CREG_moorings.py
+++ CREG_moorings.py
@@ -37,43 +37,43 @@ do_Kprofile=True     # Read a previous profile to compare the current one
 ########################################
 #------------------------------------------------------------------------------------------------------------------------
 # Read only once for the same section type
-print
-print ''
-print '    >>>>>>>>>>>>>>>>>>>>>>>>>>>>Perform the treatment of the '+CONFCASE+' experiment '
-print ''
-print
-print '		Start to read geographical, scale factors & masks '
-print
+print()
+print()
+print('      >>>>>>>>>>>>>>>>>>>>>>>>>>>>Perform the treatment of the '+CONFCASE+' experiment ')
+print()
+print()
+print('         Start to read geographical, scale factors & masks ')
+print()
 
 locpath=grid_dir
 locfile=CONFCASE+'_mask.nc'
 if chkfile(locpath+locfile,zstop=True,zscript=sys.argv[0]) :
-	fieldmask=Dataset(locpath+locfile)
-	tmask = npy.squeeze(fieldmask.variables['tmask'])
+    fieldmask=Dataset(locpath+locfile)
+    tmask = npy.squeeze(fieldmask.variables['tmask'])
 
 locpath=grid_dir
 locfile=CONFCASE+'_mesh_hgr.nc'
 if chkfile(locpath+locfile,zstop=True,zscript=sys.argv[0]) :
-	fieldhmesh=Dataset(locpath+locfile)
-	lon = npy.squeeze(fieldhmesh.variables['glamt'])
-	lat = npy.squeeze(fieldhmesh.variables['gphit'])
-	e1t= npy.squeeze(fieldhmesh.variables['e1t'])
-	e2t= npy.squeeze(fieldhmesh.variables['e2t'])
+    fieldhmesh=Dataset(locpath+locfile)
+    lon = npy.squeeze(fieldhmesh.variables['glamt'])
+    lat = npy.squeeze(fieldhmesh.variables['gphit'])
+    e1t= npy.squeeze(fieldhmesh.variables['e1t'])
+    e2t= npy.squeeze(fieldhmesh.variables['e2t'])
 
 locpath=grid_dir
 locfile=CONFCASE+'_mesh_zgr.nc'
 if chkfile(locpath+locfile,zstop=True,zscript=sys.argv[0]) :
-	fieldzmesh=Dataset(locpath+locfile)
-	e3t = npy.squeeze(fieldzmesh.variables['e3t_0'])
+    fieldzmesh=Dataset(locpath+locfile)
+    e3t = npy.squeeze(fieldzmesh.variables['e3t_0'])
 
 name_z='deptht'
 
 locpath=data_dir+'/'+str(s_year)+'/'
 locfile=CONFCASE+'_y'+str(s_year)+'.'+xiosfreq+'_gridT.nc'
 if chkfile(locpath+locfile,zstop=True,zscript=sys.argv[0]) :
-	z = PyRaf.readfullNC4(locpath+locfile,name_z)
+    z = PyRaf.readfullNC4(locpath+locfile,name_z)
 z2dt=npy.reshape(z,(z.size,1))
-print '		End to read geographical, scale factors & masks '
+print('         End to read geographical, scale factors & masks ')
 
 # Define time axis
 if monthly_data == 1:
@@ -91,7 +91,7 @@ else:
         time_axis=npy.arange(e_year-s_year+1)+s_year+0.5
 
 if monthly_data == 1 and e_year-s_year == 0:
-	time_axis=npy.arange(12)+1.5
+    time_axis=npy.arange(12)+1.5
 
 xplt= npy.tile(time_axis,(z.size,1))
 zplt = npy.repeat(z2dt,time_axis.shape[0],axis=1)
@@ -108,7 +108,7 @@ else:
         time_dim=e_year-s_year+1
 
 if time_dim == 0:
-	time_dim = 1
+    time_dim = 1
 
 nbvar = 2 
 Ar_size=(nbvar,time_dim,e3t.shape[0],lon.shape[0],lon.shape[1])
@@ -117,61 +117,61 @@ Ar_size=(nbvar,e3t.shape[0],lon.shape[0],lon.shape[1])
 My_varinit=npy.empty(Ar_size)
 
 if read_IAinputdata != 1:
-	Ar_size=(time_dim,e3t.shape[0],lon.shape[0],lon.shape[1])
-	My_varTemp=npy.empty(Ar_size)
+    Ar_size=(time_dim,e3t.shape[0],lon.shape[0],lon.shape[1])
+    My_varTemp=npy.empty(Ar_size)
 #del lat
 #del lon
 
 
 nvar=0
 for var in DEF_INFO_VAR('gridT','votemper'):
-	t_month=0
+    t_month=0
 
-	if read_IAinputdata == 1:
-		print 
-		print 'Start to read ', var['name'],' data '
-		print 
-		# Read the whole time series
-		c_year=s_year
-		while c_year <= e_year:
-               		if monthly_data == 1:
-               		    c_month=0
-               		    while c_month <= 11:
-				smonth='m0'+str(c_month+1) if c_month <= 8 else 'm'+str(c_month+1)
-        			locpath=data_dir+'/'+str(c_year)+'/'
-        			locfile=CONFCASE+'_y'+str(c_year)+smonth+'.'+xiosfreq+'_gridT.nc'
-				if chkfile(locpath+locfile) :
-				 	My_var[nvar,t_month,:,:,:] = PyRaf.readfullNC4(locpath+locfile,var['name'])
-                                t_month+=1
-                                c_month+=1
-				print 'Read year :', c_year, ' month:', c_month
-			else:
-        		    locpath=data_dir+'/'+str(c_year)+'/'
-        		    locfile=CONFCASE+'_y'+str(c_year)+'.'+xiosfreq+'_gridT.nc'
-			    if chkfile(locpath+locfile) :
-			    	My_var[nvar,c_year-s_year,:,:,:] = PyRaf.readfullNC4(locpath+locfile,var['name'])
-			    print 'Read year :', c_year
+    if read_IAinputdata == 1:
+        print()
+        print('  Start to read '+ var['name']+' data ')
+        print() 
+        # Read the whole time series
+        c_year=s_year
+        while c_year <= e_year:
+                    if monthly_data == 1:
+                        c_month=0
+                        while c_month <= 11:
+                            smonth='m0'+str(c_month+1) if c_month <= 8 else 'm'+str(c_month+1)
+                            locpath=data_dir+'/'+str(c_year)+'/'
+                            locfile=CONFCASE+'_y'+str(c_year)+smonth+'.'+xiosfreq+'_gridT.nc'
+                            if chkfile(locpath+locfile) :
+                                My_var[nvar,t_month,:,:,:] = PyRaf.readfullNC4(locpath+locfile,var['name'])
+                            t_month+=1
+                            c_month+=1
+                            print('  Read year :'+ str(c_year)+ ' month:'+ str(c_month))
+                    else:
+                            locpath=data_dir+'/'+str(c_year)+'/'
+                            locfile=CONFCASE+'_y'+str(c_year)+'.'+xiosfreq+'_gridT.nc'
+                            if chkfile(locpath+locfile) :
+                                My_var[nvar,c_year-s_year,:,:,:] = PyRaf.readfullNC4(locpath+locfile,var['name'])
+                            print('  Read year :'+ str(c_year))
 
-			c_year = c_year + 1 
-	else:
-        	locpath='./'
-        	locfile=CONFCASE+'_gridT_'+var['shortname']+'_IA.npy'
-		if chkfile(locpath+locfile) :
-			print 'Reload data from:', locpath+locfile
-			My_varTemp[:,:,:,:]=npy.load(locpath+locfile,mmap_mode='r')
-                	My_var[nvar,:,:,:,:]=My_varTemp[:,:,:,:].copy()
-			print 'Check shape of reloaded data array:', My_var.shape
-			#print 'Reloaded data max value:', My_var.max()
-			#print 'Reloaded data min value:', My_var.min()
+                    c_year = c_year + 1 
+    else:
+        locpath='./'
+        locfile=CONFCASE+'_gridT_'+var['shortname']+'_IA.npy'
+        if chkfile(locpath+locfile) :
+            print('  Reload data from:'+ locpath+locfile)
+            My_varTemp[:,:,:,:]=npy.load(locpath+locfile,mmap_mode='r')
+            My_var[nvar,:,:,:,:]=My_varTemp[:,:,:,:].copy()
+            print('  Check shape of reloaded data array:'+ My_var.shape)
+            #print(' Reloaded data max value:'+ str(My_var.max())
+            #print(' Reloaded data min value:'+ str(My_var.min())
 
-	# Read the initial state 
-        locpath=data_dir+'/'
-        locfile=CONFCASE+'_init_gridT.nc'
-	if chkfile(locpath+locfile) :
-		My_varinit[0,:,:,:] = PyRaf.readfullNC4(locpath+locfile,'votemper')
-		My_varinit[1,:,:,:] = PyRaf.readfullNC4(locpath+locfile,'vosaline')
+    # Read the initial state 
+    locpath=data_dir+'/'
+    locfile=CONFCASE+'_init_gridT.nc'
+    if chkfile(locpath+locfile) :
+        My_varinit[0,:,:,:] = PyRaf.readfullNC4(locpath+locfile,'votemper')
+        My_varinit[1,:,:,:] = PyRaf.readfullNC4(locpath+locfile,'vosaline')
 
-	nvar+=1
+    nvar+=1
 
 ####################################################################################################################
 ####################################################################################################################
@@ -180,30 +180,30 @@ for var in DEF_INFO_VAR('gridT','votemper'):
 ####################################################################################################################
 #if False: 
 if lgTS_ye-lgTS_ys+1 > 1 :
-        plt.clf()
-	plt.subplots_adjust(bottom=0.1, left=0.1, right=0.95, top=0.9)
-	num_fram=410
-	All_box=XXALLBOXXX
-	All_var=DEF_INFO_VAR('gridT','votemper')
-	cumul_box=''
-	moor_n=1
-        for zmybox in All_box :
-		zbox=DEF_MOOR_BOX(CONFIG,zmybox)
-		hsct_lev,Red_My_varinit=DEF_REDVAR(CONFIG,CASE,My_var,My_varinit,zbox,nbvar,time_dim,e3t,All_var,s_year,e_year,tmask,lon,lat,e1t3D,e2t3D)
-		fram=num_fram+moor_n
-		DEF_ZTIME(CONFIG,CASE,lgTS_ys,lgTS_ye,zbox,z,z2dt,hsct_lev,zfram=fram,zoutNC=NCDF_OUT) 
-		cumul_box=cumul_box+'-'+zbox['box']
-		moor_n+=2
+    plt.clf()
+    plt.subplots_adjust(bottom=0.1, left=0.1, right=0.95, top=0.9)
+    num_fram=410
+    All_box=XXALLBOXXX
+    All_var=DEF_INFO_VAR('gridT','votemper')
+    cumul_box=''
+    moor_n=1
+    for zmybox in All_box :
+        zbox=DEF_MOOR_BOX(CONFIG,zmybox)
+        hsct_lev,Red_My_varinit=DEF_REDVAR(CONFIG,CASE,My_var,My_varinit,zbox,nbvar,time_dim,e3t,All_var,s_year,e_year,tmask,lon,lat,e1t3D,e2t3D)
+        fram=num_fram+moor_n
+        DEF_ZTIME(CONFIG,CASE,lgTS_ys,lgTS_ye,zbox,z,z2dt,hsct_lev,zfram=fram,zoutNC=NCDF_OUT) 
+        cumul_box=cumul_box+'-'+zbox['box']
+        moor_n+=2
 
-	# Save year time series to use for a longer time series
-	timeadd='_month' if monthly_data == 1 else ''
+    # Save year time series to use for a longer time series
+    timeadd='_month' if monthly_data == 1 else ''
 
-	file_ext1='_TemSalEvol'+timeadd
-	file_ext=file_ext1+cumul_box+'_LGTS_y'+str(lgTS_ys)+'LASTy'
-	file_name=CONFCASE+file_ext
+    file_ext1='_TemSalEvol'+timeadd
+    file_ext=file_ext1+cumul_box+'_LGTS_y'+str(lgTS_ys)+'LASTy'
+    file_name=CONFCASE+file_ext
 
-	plt.tight_layout()
-        plt.savefig(DIR_FIG_OUT+file_name+'.png',dpi=300)
+    plt.tight_layout()
+    plt.savefig(DIR_FIG_OUT+file_name+'.png',dpi=300)
 
 
 ####################################################################################################################
@@ -212,35 +212,28 @@ if lgTS_ye-lgTS_ys+1 > 1 :
 ####################################################################################################################
 ####################################################################################################################
 if do_Kprofile :
-	print
-	print " 			##################################################################  " 
-	print " 			##################################################################  " 
-	print " 			######### PLOT VERTICAL PROFILES AT MOORINGS LOCATION ############  " 
-	print " 			##################################################################  " 
-	print " 			##################################################################  " 
-	print
-        plt.clf()
-	num_fram=220
-	All_box=XXALLBOXXX
-	All_var=DEF_INFO_VAR('gridT','votemper')
-	cumul_box=''
-	moor_n=1
-        for zmybox in All_box :
-		zbox=DEF_MOOR_BOX(CONFIG,zmybox)
-		fram=num_fram+moor_n
-		hsct_lev,Red_My_varinit=DEF_REDVAR(CONFIG,CASE,My_var,My_varinit,zbox,nbvar,time_dim,e3t,All_var,s_year,e_year,tmask,lon,lat,e1t3D,e2t3D)
-		DEF_ZPROFILE(CONFIG,CASE,str(s_year),hsct_lev,Red_My_varinit,zbox,zplt,zfram=fram,zs_year=s_year) 
-		cumul_box=cumul_box+'-'+zbox['box']
-		moor_n+=2
+    print()
+    print('             ##################################################################  ')
+    print('             ##################################################################  ')
+    print('             ######### PLOT VERTICAL PROFILES AT MOORINGS LOCATION ############  ')
+    print('             ##################################################################  ')
+    print('             ##################################################################  ')
+    print()
+    plt.clf()
+    num_fram=220
+    All_box=XXALLBOXXX
+    All_var=DEF_INFO_VAR('gridT','votemper')
+    cumul_box=''
+    moor_n=1
+    for zmybox in All_box :
+        zbox=DEF_MOOR_BOX(CONFIG,zmybox)
+        fram=num_fram+moor_n
+        hsct_lev,Red_My_varinit=DEF_REDVAR(CONFIG,CASE,My_var,My_varinit,zbox,nbvar,time_dim,e3t,All_var,s_year,e_year,tmask,lon,lat,e1t3D,e2t3D)
+        DEF_ZPROFILE(CONFIG,CASE,str(s_year),hsct_lev,Red_My_varinit,zbox,zplt,zfram=fram,zs_year=s_year) 
+        cumul_box=cumul_box+'-'+zbox['box']
+        moor_n+=2
 
         pfile_ext='_TemSaliProfile'+cumul_box+'_y'+str(s_year)+'_z0-500'
         file_name=CONFIG+'-'+CASE+pfile_ext
-	plt.tight_layout()
+        plt.tight_layout()
         plt.savefig(DIR_FIG_OUT+file_name+'.png',dpi=300)
-
-
-
-
-
-
-
